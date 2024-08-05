1ba0b1b58ffa795c73a2e3fd259520a31e3ec8bb
esraaaatmeh <joudjarrah175@gmail.com>
2024-08-05 13:16:17 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -14,21 +14,34 @@ class MyApp extends StatelessWidget{
 
        appBar: AppBar(
          backgroundColor: Colors.blueGrey,
-         title: Container(child:Text("App Bar"),
+         title: Container(child:Text("Flutter course"),
            alignment: Alignment.center,color: Colors.indigoAccent,),
+       ),
+       body:
+       Scrollbar(
+         thumbVisibility: true,
+
+         child:ListView(children:
+          [Column(children:[Text("Hello",style:TextStyle(color:Colors.white10,),),
+         Container(child: Text("First course \"Dart\"",style:
+         TextStyle(fontSize:30)),
+           height: 200,alignment: Alignment.center,),
+         //any element in col take center position by default
+         Container(child:Image.asset('assets/Dart-logo.png',fit: BoxFit.contain,),),
+               Container(child: Text("Second course",style: TextStyle(fontSize: 30),),height: 200,alignment: Alignment.center,),
+               Container(child:Image.asset('assets/flutter.jpg',fit: BoxFit.contain,),),
+          Container(child: Text(""),height: 200,)],
+               crossAxisAlignment: CrossAxisAlignment.start,),
+             ],)
 
        ),
-       body: Column(children:[ Text("Hello this is a body of Scaffold"),
-         Container(child:Image.asset('assets/flutter.jpg',fit: BoxFit.contain,),)
-]),
+         backgroundColor: Colors.cyanAccent,
+         floatingActionButton:  FloatingActionButton.extended(onPressed: () {print("klk");},
+           label: Text("add"),
+           icon: Icon(Icons.add_circle),)
 
-       backgroundColor: Colors.cyanAccent,
-       floatingActionButton:  FloatingActionButton.extended(onPressed: () {print("klk");},
-         label: Text("add"),
-         icon: Icon(Icons.add_circle),)
-     ),
-     );
 
-    
-  }
+),
+);
+}
 }
